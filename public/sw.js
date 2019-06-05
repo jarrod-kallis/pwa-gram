2ef1e0a10fd22c9bc078b30e2edf4a218cc7e379
Jarrod Kallis <jarrod.kallis@gmail.com>
2019-06-05 16:41:06 +0200

    Service worker lifecycle events

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+// Fired when the browser installs the service worker
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Installing Service Worker...', event);
+});
+
+// Fired when the browser activates the service worker
+// This event won't fire if you have existing instances of your app open in the browser.
+// Check 'Application' tab in Chrome Dev Tools to see if a new service worker is waiting to activate.
+// Shut down all tabs where app is running and re-open them. Just saw that Ctrl+F5 works as well.
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activating Service Worker...', event);
+  return self.clients.claim();
+});
