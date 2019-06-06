0cd8a96466480206dd1d7b80fee976cf829a3a39
Jarrod Kallis <jarrod.kallis@gmail.com>
2019-06-06 14:33:31 +0200

    Static asset caching (precaching)

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,34 @@
 // Fired when the browser installs the service worker
 self.addEventListener('install', event => {
   // console.log('[Service Worker] Installing Service Worker...', event);
+  // Earliest time to cache assets
+  // Must wait until everything is cached before finishing up the installation
+  event.waitUntil(
+    caches.open('static').then(cache => {
+      console.log('[Service Worker] Precaching the app shell');
+      // cache.add('/');
+      // cache.add('/index.html');
+      // cache.add('/src/js/app.js');
+      // cache.add('/src/js/feed.js');
+
+      cache.addAll([
+        // '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+        // Not necessary to cache these polyfills
+        '/src/js/promise.js',
+        '/src/js/fetch.js'
+      ]);
+    })
+  );
 });
 
 // Fired when the browser activates the service worker
@@ -18,5 +46,14 @@ self.addEventListener('fetch', event => {
   // }
 
   // event.respondWith(null);
-  event.respondWith(fetch(event.request));
+  // event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
+    })
+  );
 });
